b3e17ccf6a8bbb326910d1bd2fc14768f0b29a9f
jasonjohn12 <jasonjohn12@gmail.com>
2021-11-25 20:38:51 -0600

    wrapped emjoi in a span

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,10 @@ export default function Home({ posts }) {
       <div className="flex flex-col items-center my-6 xl:flex-row gap-x-12 xl:mb-12">
         <div className="pt-6">
           <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, I’m Jason. 👋
+            Hi, I’m Jason.{' '}
+            <span role="img" aria-label="donut">
+              👋
+            </span>
           </h1>
           <h2 className="text-lg prose text-gray-600 dark:text-gray-400">
             {`${siteMetadata.description}. `}
