46f31afecab240642f6c456995bad6cbd32ed293
jasonjohn12 <jasonjohn12@gmail.com>
2021-11-25 20:46:23 -0600

    show brand in mobile

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -17,7 +17,7 @@ const LayoutWrapper = ({ children }) => {
               <div className="flex items-center justify-between">
                 <div className="mr-3">{/* <Logo /> */}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-medium sm:block">
+                  <div className=" h-6 text-2xl font-medium sm:block">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
